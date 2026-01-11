25108a4014e3cf155f8e8a2183a5384aac914abb
Koh Jun Jie <koh.jun.jie@outlook.com>
2026-01-11 17:20:51 +0800

    completed infra file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,36 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "aws_linux" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["al2023-ami-*-x86_64"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["amazon"]
 }
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "protohacker_server" {
+  ami           = data.aws_ami.aws_linux.id
   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.private_subnets[0]
+  key_name = "jjkoh-mbp-aws"
+
+  vpc_security_group_ids      = [aws_security_group.protohackers-smoke-test-security-group.id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  availability_zone           = "ap-southeast-1a"
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size           = 10
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
 
   tags = {
-    Name = "learn-terraform"
+    Name = "protohackers-smoke-test"
   }
 }
 
@@ -29,7 +39,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.6.0"
 
-  name = "example-vpc"
+  name = "protohackers-smoke-test-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
@@ -37,4 +47,44 @@ module "vpc" {
   public_subnets  = ["10.0.128.0/18", "10.0.192.0/18"]
 
   enable_dns_hostnames = true
+
+  create_igw         = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+}
+
+resource "aws_security_group" "protohackers-smoke-test-security-group" {
+  name        = "protohackers_smoke_test_security_group"
+  description = "Allow ssh, inbound TCP on port 8090 and all outbound TCP requests"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "protohackers smoke test security group+"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.protohackers-smoke-test-security-group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 8090
+  ip_protocol = "tcp"
+  to_port     = 8090
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.protohackers-smoke-test-security-group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic" {
+  security_group_id = aws_security_group.protohackers-smoke-test-security-group.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
